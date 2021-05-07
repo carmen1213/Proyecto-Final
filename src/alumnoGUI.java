848575a2347b49ec283519766ed5685562617d2a
carma <cmarti@campuscamarafp.com>
2021-05-07 23:53:29 +0200

    interfaz de alumno son acabar

--- src/alumnoGUI.java
+++ src/alumnoGUI.java
@@ -9,6 +9,11 @@ import static java.awt.Font.BOLD;
 public class alumnoGUI extends JFrame {
 
     private JLabel titulop;
+    private JLabel icon;
+    private JButton notas;
+    private JButton horario;
+    private JLabel vacioderecha;
+    private JLabel vacioizquierda;
     Font f = new Font("Monospaced", BOLD, 30);
 
 
@@ -43,7 +48,7 @@ public class alumnoGUI extends JFrame {
         setJMenuBar(barraMenu);
 
         JPanel principal = new JPanel();
-        principal.setLayout(new GridLayout(4, 1));
+        principal.setLayout(new GridLayout(3, 1));
         principal.setBackground(new Color(179, 253, 255));
 
         JPanel titulo = new JPanel();
@@ -53,12 +58,66 @@ public class alumnoGUI extends JFrame {
         titulop.setForeground(new Color(140, 24, 82));
         titulo.add(titulop);
 
+        JPanel logo = new JPanel();
+        logo.setBackground(new Color(179, 253, 255));
+        icon = new JLabel();
+        icon.setBackground(new Color(179, 253, 255));
+        icon.setIcon(new ImageIcon("C:\\Users\\carma\\IdeaProjects\\Trabajo final\\imagenes\\alumno.jpg"));
 
+        JPanel botones = new JPanel();
+        botones.setBackground(new Color(179, 253, 255));
+        botones.setLayout(new GridLayout(1,4));
+
+        JPanel vacioiz = new JPanel();
+        vacioiz.setBackground(new Color(179, 253, 255));
+        vacioizquierda = new JLabel();
+        vacioizquierda.setBackground(new Color(179, 253, 255));
+
+        JPanel nota = new JPanel();
+        nota.setLayout(new GridLayout(3,1));
+        nota.setBackground(new Color(179, 253, 255));
+        JLabel vacioarr = new JLabel();
+        notas = new JButton("Notas");
+        JLabel vacioaba = new JLabel();
+
+        JPanel vaciode = new JPanel();
+        vaciode.setBackground(new Color(179, 253, 255));
+        vacioderecha = new JLabel();
+        vacioderecha.setBackground(new Color(179, 253, 255));
+
+        JPanel Horario = new JPanel();
+        Horario.setLayout(new GridLayout(3,1));
+        Horario.setBackground(new Color(179, 253, 255));
+        JLabel vacioar = new JLabel();
+        horario = new JButton("Horario");
+        horario.setSize(3,4);
+        JLabel vacioab = new JLabel();
+
+
+        vacioiz.add(vacioizquierda);
+        nota.add(vacioarr);
+        nota.add(notas);
+        nota.add(vacioaba);
+        vaciode.add(vacioderecha);
+        Horario.add(vacioar);
+        Horario.add(horario);
+        Horario.add(vacioab);
+
+
+        botones.add(nota);
+        botones.add(vacioiz);
+        botones.add(vaciode);
+        botones.add(Horario);
+
+
+        logo.add(icon);
         principal.add(titulo);
+        principal.add(logo);
+        principal.add(botones, BorderLayout.SOUTH);
         general.add(principal);
         add(general);
 
-        setSize(650, 600);
+        setSize(700, 700);
         setVisible(true);
     }
 
