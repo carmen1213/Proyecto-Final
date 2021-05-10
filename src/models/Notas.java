92ab0050a0b14325fdc8c6b2e864011f180560f9
Dam lucia <lcala419@gmail.com>
2021-05-10 11:01:05 +0200

    Inicio de la clase notas

--- src/models/Notas.java
+++ src/models/Notas.java
@@ -1,18 +1,28 @@
 package models;
 
 public class Notas {
-    int nota;
+    String nombre_alumno;
+    String asignatura;
 
-    public int getNota() {
-        return nota;
+
+    public String getNombre_alumno() {
+        return nombre_alumno;
     }
 
-    public void setNota(int nota) {
-        this.nota = nota;
+    public void setNombre_alumno(String nombre_alumno) {
+        this.nombre_alumno = nombre_alumno;
     }
 
-    public Notas(int nota) {
-        this.nota = nota;
+    public String getAsignatura() {
+        return asignatura;
+    }
 
+    public void setAsignatura(String asignatura) {
+        this.asignatura = asignatura;
+    }
+
+    public Notas(String nombre_alumno, String asignatura) {
+        this.nombre_alumno = nombre_alumno;
+        this.asignatura = asignatura;
     }
 }
