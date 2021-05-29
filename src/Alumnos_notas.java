1193b3c7352f57a6065cef7ca33168e16c167b8b
lucia816 <73232190+lucia816@users.noreply.github.com>
2021-05-29 20:26:41 +0200

    Arrego bug de notas alumnos

--- src/Alumnos_notas.java
+++ src/Alumnos_notas.java
@@ -22,92 +22,88 @@ public class Alumnos_notas extends JFrame {
     private ControladorNotasALumnos controladorTablanotasAlumnos = new ControladorNotasALumnos();
     private JLabel titulop;
     private Controlador_login controllogin = new Controlador_login();
-    private Profesor id_profesor = controllogin.getIdProfesor(inicio.usuario.getText(), inicio.contraseña.getText());
-    private ArrayList<Asignatura> asignaturas = controladorProfesores.getAsignaturaProfesor(2);
-    ArrayList<Integer> notitas = controladorTablanotasAlumnos.getNotasalumnos();
-    private UtilDateModel date = new UtilDateModel();
-    private static Connection conn;
+    private ArrayList<Integer> notitas = controladorTablanotasAlumnos.getNotasalumnos(controllogin.getIdalumno(inicio.usuario.getText(),inicio.contraseña.getText()).getId());
+
 
     public Alumnos_notas() throws SQLException {
-          super("Listas");
-    setDefaultCloseOperation(HIDE_ON_CLOSE);
+        super("Listas");
+        setDefaultCloseOperation(HIDE_ON_CLOSE);
         this.getContentPane().setBackground(new Color(227, 247, 193));
 
 
-    JPanel general = new JPanel();
+        JPanel general = new JPanel();
         general.setLayout(new GridLayout(5, 1));
         general.setBackground(new Color(227, 247, 193));
 
-    JPanel titulo = new JPanel();
+        JPanel titulo = new JPanel();
         titulo.setBackground(new Color(227, 247, 193));
-    titulop = new JLabel("Bienvenido");
+        titulop = new JLabel("Bienvenido");
         titulo.add(titulop);
 
-    JPanel opciones = new JPanel();
+        JPanel opciones = new JPanel();
         opciones.setBackground(new Color(227, 247, 193));
         opciones.setLayout(new GridLayout(1, 4, 12, 0));
         opciones.add(new JLabel(" "));
         opciones.add(new JLabel(" "));
 
-        ArrayList<Alumno> alumnosAsignatura = controladorProfesores.getAlumnosxAsignaturaA(3);
+        ArrayList<Asignatura> asignaturas = controladorTablanotasAlumnos.getasignaturas(inicio.usuario.getText(),inicio.contraseña.getText(),3);
+        System.out.println(asignaturas);
+        String alumnosAsignatura = controllogin.getIdalumno(inicio.usuario.getText(),inicio.contraseña.getText()).getNombre();
+        System.out.println(alumnosAsignatura);
+        DefaultTableModel model = generarModeloTablaNotasProfesor(alumnosAsignatura, asignaturas, notitas);
 
-        DefaultTableModel model = generarModeloTablaNotasProfesor(alumnosAsignatura,asignaturas,notitas);
+        jTableAlumnos = new JTable(model) {
+            //private static final long serialVersionUID = 1L;
+            @Override
+            public Class getColumnClass(int column) {
+                switch (column) {
+                    case 0:
+                        return String.class;
+                    case 1:
+                        return String.class;
+                    default:
+                    case 2:
+                        return String.class;
 
-    jTableAlumnos = new JTable(model) {
-        //private static final long serialVersionUID = 1L;
-        @Override
-        public Class getColumnClass(int column) {
-            switch (column) {
-                case 0:
-                    return String.class;
-                case 1:
-                    return String.class;
-                default:
-                case 2:
-                    return String.class;
-
+                }
             }
-        }
-    };
+        };
 
 
-        jTableAlumnos.setBounds(30,40,200,300);
-    JScrollPane scrollpane = new JScrollPane(jTableAlumnos);
+        jTableAlumnos.setBounds(30, 40, 200, 300);
+        JScrollPane scrollpane = new JScrollPane(jTableAlumnos);
 
-
-    JPanel primer = new JPanel();
-        primer.setLayout(new GridLayout(2,1));
+        JPanel primer = new JPanel();
+        primer.setLayout(new GridLayout(2, 1));
         primer.setBackground(new Color(227, 247, 193));
-    JPanel referencia = new JPanel();
+        JPanel referencia = new JPanel();
         referencia.setBackground(new Color(227, 247, 193));
         referencia.add(new JLabel(" "));
 
-    JPanel boton = new JPanel();
+        JPanel boton = new JPanel();
         boton.setBackground(new Color(227, 247, 193));
-        boton.setLayout(new GridLayout(1,3));
-    JLabel vacioar = new JLabel(" ");
+        boton.setLayout(new GridLayout(1, 3));
+        JLabel vacioar = new JLabel(" ");
         vacioar.setBackground(new Color(227, 247, 193));
-    JLabel vacioab = new JLabel(" ");
+        JLabel vacioab = new JLabel(" ");
         vacioab.setBackground(new Color(227, 247, 193));
 
-    JPanel guarda = new JPanel();
-        guarda.setLayout(new GridLayout(3,1));
-    JLabel vacioarriba = new JLabel(" ");
+        JPanel guarda = new JPanel();
+        guarda.setLayout(new GridLayout(3, 1));
+        JLabel vacioarriba = new JLabel(" ");
         vacioar.setBackground(new Color(227, 247, 193));
-    JLabel vacioabajo = new JLabel(" ");
+        JLabel vacioabajo = new JLabel(" ");
         vacioab.setBackground(new Color(227, 247, 193));
 
-    JPanel botones =new JPanel();
+        JPanel botones = new JPanel();
         botones.setBackground(new Color(227, 247, 193));
-    JLabel vacioarr = new JLabel(" ");
+        JLabel vacioarr = new JLabel(" ");
         vacioar.setBackground(new Color(227, 247, 193));
         guarda.setBackground(new Color(227, 247, 193));
 
 
-
-    JLabel vacioaba = new JLabel(" ");
+        JLabel vacioaba = new JLabel(" ");
         vacioab.setBackground(new Color(227, 247, 193));
-
 
 
         botones.add(vacioarr);
@@ -122,7 +118,6 @@ public class Alumnos_notas extends JFrame {
         boton.add(guarda);
 
 
-
         primer.add(referencia);
 
         general.add(titulo);
@@ -131,31 +126,27 @@ public class Alumnos_notas extends JFrame {
         general.add(primer);
         general.add(boton);
 
-    add(general,BorderLayout.CENTER);
+        add(general, BorderLayout.CENTER);
 
 
-    setSize(860, 530);
-    setVisible(true);
+        setSize(860, 530);
+        setVisible(true);
 
-}
+    }
 
-    private DefaultTableModel generarModeloTablaNotasProfesor(ArrayList<Alumno> alumnosAsignatura, ArrayList<Asignatura> asignaturas, ArrayList<Integer> notitas) {
+    private DefaultTableModel generarModeloTablaNotasProfesor(String alumnosAsignatura, ArrayList<Asignatura> asignaturas, ArrayList<Integer> notitas) {
         String[] cols = {"Nombre Alumno", "Nombre Asignatura", "Notas"};
         DefaultTableModel model = new DefaultTableModel(cols, 0);
 
-
         //Ciclo para ingresar los nombres de los alumnos.
-        for (int j = 0; j < alumnosAsignatura.size(); j++) {
-            for (int i = 0; i < asignaturas.size(); i++) {
-                for (int k = 0; k < notitas.size(); k++) {
-                    Object[] data = {alumnosAsignatura.get(j).getNombre(), asignaturas.get(i).getNombre(), notitas.get(k)};
-                    model.addRow(data);
-                }
 
-            }
+
+            for (int k = 0; k < notitas.size(); k++) {
+                    Object[] data = {alumnosAsignatura, asignaturas.get(k).getNombre(),notitas.get(k) };
+                    model.addRow(data);
         }
 
         return model;
     }
-    }
+}
 
