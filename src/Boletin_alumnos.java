388b7ff1bf79eacb7eb35ac54b00f27080a21a4c
Dam lucia <lcala419@gmail.com>
2021-06-03 09:36:06 +0200

    Arreglo bug de notas de profesores

--- src/Boletin_alumnos.java
+++ src/Boletin_alumnos.java
@@ -3,8 +3,8 @@ import Controladores.ControladorTablaNotas;
 import Controladores.ControladorTablaProfesores;
 import Controladores.Controlador_login;
 import Interfaz.Metodos_repetitivos;
-import models.*;
-
+import models.Alumno;
+import models.Asignatura;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-public class Boletin_alumnos extends JFrame{
+public class Boletin_alumnos extends JFrame {
     private JTable jTableAlumnos;
     private ControladorTablaNotas controladorNotas = new ControladorTablaNotas();
     private ControladorTablaProfesores controladorProfesores = new ControladorTablaProfesores();
@@ -24,6 +24,10 @@ public class Boletin_alumnos extends JFrame{
     private ArrayList<Asignatura> asignaturas = controladorProfesores.getAsignaturaProfesor(controllogin.getIdProfesor(inicio.usuario.getText(), inicio.contraseña.getText()).getId_profesor());
     private static Connection conn;
 
+    /**
+     *
+     * @throws SQLException
+     */
     public Boletin_alumnos() throws SQLException {
         super("Listas");
         setDefaultCloseOperation(HIDE_ON_CLOSE);
@@ -43,10 +47,10 @@ public class Boletin_alumnos extends JFrame{
 
 
         ArrayList<Alumno> alumnosAsignatura = controladorProfesores.getAlumnosxAsignaturaA(controladorNotas.getid_asignatura(controllogin.getIdProfesor(inicio.usuario.getText(), inicio.contraseña.getText()).getId_profesor()));
-        DefaultTableModel model = generarModeloTablaAlumno(alumnosAsignatura,asignaturas);
+        DefaultTableModel model = generarModeloTablaAlumno(alumnosAsignatura, asignaturas);
+
 
         jTableAlumnos = new JTable(model) {
-            //private static final long serialVersionUID = 1L;
             @Override
             public Class getColumnClass(int column) {
                 switch (column) {
@@ -63,12 +67,12 @@ public class Boletin_alumnos extends JFrame{
         };
 
 
-        jTableAlumnos.setBounds(30,40,200,300);
+        jTableAlumnos.setBounds(30, 40, 200, 300);
         JScrollPane scrollpane = new JScrollPane(jTableAlumnos);
 
 
         JPanel primer = new JPanel();
-        primer.setLayout(new GridLayout(2,1));
+        primer.setLayout(new GridLayout(2, 1));
         primer.setBackground(new Color(227, 247, 193));
         JPanel referencia = new JPanel();
         referencia.setBackground(new Color(227, 247, 193));
@@ -76,31 +80,30 @@ public class Boletin_alumnos extends JFrame{
 
         JPanel boton = new JPanel();
         boton.setBackground(new Color(227, 247, 193));
-        boton.setLayout(new GridLayout(1,3));
+        boton.setLayout(new GridLayout(1, 3));
         JLabel vacioar = new JLabel(" ");
         vacioar.setBackground(new Color(227, 247, 193));
         JLabel vacioab = new JLabel(" ");
         vacioab.setBackground(new Color(227, 247, 193));
 
         JPanel guarda = new JPanel();
-        guarda.setLayout(new GridLayout(3,1));
+        guarda.setLayout(new GridLayout(3, 1));
         JLabel vacioarriba = new JLabel(" ");
         vacioar.setBackground(new Color(227, 247, 193));
         JLabel vacioabajo = new JLabel(" ");
         vacioab.setBackground(new Color(227, 247, 193));
 
-        JPanel botones =new JPanel();
+        JPanel botones = new JPanel();
         botones.setBackground(new Color(227, 247, 193));
         JLabel vacioarr = new JLabel(" ");
         vacioar.setBackground(new Color(227, 247, 193));
         guarda.setBackground(new Color(227, 247, 193));
 
         guardar = new JButton("Guardar");
-        guardar.addActionListener(new Guardar(this ));
+        guardar.addActionListener(new Guardar(this));
 
         JLabel vacioaba = new JLabel(" ");
         vacioab.setBackground(new Color(227, 247, 193));
-
 
 
         botones.add(vacioarr);
@@ -116,16 +119,15 @@ public class Boletin_alumnos extends JFrame{
         boton.add(guarda);
 
 
-
         primer.add(referencia);
 
-        general.add(Metodos_repetitivos.getjPanel());
+        general.add(Metodos_repetitivos.Titulo());
         general.add(opciones);
         general.add(scrollpane);
         general.add(primer);
         general.add(boton);
 
-        add(general,BorderLayout.CENTER);
+        add(general, BorderLayout.CENTER);
 
 
         setSize(860, 530);
@@ -134,19 +136,17 @@ public class Boletin_alumnos extends JFrame{
     }
 
 
-
-
     private DefaultTableModel generarModeloTablaAlumno(ArrayList<Alumno> nombresAlumnos, ArrayList<Asignatura> asignaturas) {
 
         String[] cols = {"Nombre Alumno", "Nombre Asignatura", "Notas"};
         DefaultTableModel model = new DefaultTableModel(cols, 0);
         //Ciclo para ingresar los nombres de los alumnos.
-            for (int j = 0; j < nombresAlumnos.size(); j++) {
-                for (int i = 0; i < asignaturas.size(); i++) {
-                    Object[] data = {nombresAlumnos.get(j).getNombre(),asignaturas.get(i).getNombre()};
-                    model.addRow(data);
+        for (int j = 0; j < nombresAlumnos.size(); j++) {
+            for (int i = 0; i < asignaturas.size(); i++) {
+                Object[] data = {nombresAlumnos.get(j).getNombre(), asignaturas.get(i).getNombre()};
+                model.addRow(data);
 
-                }
+            }
         }
 
         return model;
@@ -154,13 +154,13 @@ public class Boletin_alumnos extends JFrame{
 
     public static void main(String[] args) {
         try {
-           Boletin_alumnos notas_alumnos = new Boletin_alumnos();
+            Boletin_alumnos boletin_alumnos = new Boletin_alumnos();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
-    public static void main()throws SQLException
-    {
+
+    public static void main() throws SQLException {
         Boletin_alumnos tabla = new Boletin_alumnos();
     }
 
@@ -170,9 +170,9 @@ public class Boletin_alumnos extends JFrame{
 
         }
     }
+
     private class Guardar implements ActionListener {
         private final Boletin_alumnos notas_alumnos;
-
 
 
         public Guardar(Boletin_alumnos notas_alumnos) {
@@ -181,34 +181,37 @@ public class Boletin_alumnos extends JFrame{
 
         public void actionPerformed(ActionEvent e) {
 
-                {
-                    conexionbasedatos conexion;
-                    conexion = new conexionbasedatos();
-                    notas_alumnos.conn = conexion.conectarMySQL();
-                    Statement stmt = null;
+            {
+                conexionbasedatos conexion;
+                conexion = new conexionbasedatos();
+                notas_alumnos.conn = conexion.conectarMySQL();
+                Statement stmt = null;
+                try {
+                    stmt = notas_alumnos.conn.createStatement();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+                int ids = 0;
+                ArrayList<Asignatura> asignaturaid;
+                ArrayList<Alumno> nombresAlumnos = new ArrayList<>();
+
+                System.out.println();
+
+                Object notas = null;
+                for (int i = 0; i < jTableAlumnos.getRowCount(); i++) {
+                    notas = jTableAlumnos.getValueAt(i, 2);
+                    System.out.println(notas);
+
                     try {
-                        stmt = notas_alumnos.conn.createStatement();
+
+                        Object resultados = stmt.executeUpdate("INSERT INTO notas (notas,id_asignaturas,id_alumno) " + "VALUES('" + notas + "','" + asignaturas.get(i).getId() + "','" + controladorNotas.getidAlumno((String) jTableAlumnos.getValueAt(i,0)).getId() + "')");
+
                     } catch (SQLException throwables) {
                         throwables.printStackTrace();
                     }
-                    ArrayList<Asignatura> asignaturaid;
-
-                    Object notas = null;
-                    for (int i = 0; i < jTableAlumnos.getRowCount(); i++) {
-                        notas = jTableAlumnos.getValueAt(i, 2);
-
-                        System.out.println(notas);
-
-                        try {
-
-                                Object resultados = stmt.executeUpdate("INSERT INTO notas (notas,id_asignaturas,id_alumno) " + "VALUES('" + notas + "','" + asignaturas.get(i).getId() + "','" + 3 + "')");
-
-                        } catch (SQLException throwables) {
-                            throwables.printStackTrace();
-                        }
-                    }
-
                 }
+
             }
         }
     }
+}
--- src/Controladores/ControladorNotasALumnos.java
+++ src/Controladores/ControladorNotasALumnos.java
@@ -19,15 +19,16 @@ public class ControladorNotasALumnos {
         conexion = new conexionbasedatos();
         conn = conexion.conectarMySQL();
     }
-    public ArrayList<String> getnobreALumnos(int id){
+
+    public ArrayList<String> getnobreALumnos(int id) {
         ArrayList<String> resultado = new ArrayList<String>();
 
         String SQL = "SELECT nombre FROM alumnos_asignatura WHERE id_alumno = ?  ";
         try {
             PreparedStatement st = conn.prepareStatement(SQL);
-            st.setInt(1,id);
+            st.setInt(1, id);
             ResultSet alumnosAsignatura = st.executeQuery();
-            while (alumnosAsignatura.next()){
+            while (alumnosAsignatura.next()) {
                 String nombre = alumnosAsignatura.getString(1);
                 resultado.add(nombre);
             }
@@ -39,7 +40,7 @@ public class ControladorNotasALumnos {
         return resultado;
     }
 
-    public ArrayList<Asignatura> getasignaturas(String text, String contraseñaText, int id_alumno){
+    public ArrayList<Asignatura> getasignaturas(String text, String contraseñaText, int id_alumno) {
         ArrayList<Asignatura> resultado = new ArrayList<Asignatura>();
 
         String SQL = "SELECT nombreAsignatura, id_asignatura FROM alumnos_asignatura WHERE id_asignatura IN ( SELECT id_asignaturas FROM notas WHERE id_asignaturas = id_asignatura AND id_alumno = ?)";
@@ -47,10 +48,10 @@ public class ControladorNotasALumnos {
             PreparedStatement st = conn.prepareStatement(SQL);
             st.setInt(1, id_alumno);
             ResultSet alumnosAsignatura = st.executeQuery();
-            while (alumnosAsignatura.next()){
+            while (alumnosAsignatura.next()) {
                 String nombre = alumnosAsignatura.getString(1);
                 int id_asig = alumnosAsignatura.getInt(2);
-                resultado.add(new Asignatura(nombre,id_asig));
+                resultado.add(new Asignatura(nombre, id_asig));
                 System.out.println(resultado);
             }
 
@@ -60,8 +61,9 @@ public class ControladorNotasALumnos {
 
         return resultado;
     }
-    public ArrayList <String> getasignaturas(String nombre){
-        ArrayList <String> notas = new ArrayList<String>();
+
+    public ArrayList<String> getasignaturas(String nombre) {
+        ArrayList<String> notas = new ArrayList<String>();
 
         String SQL = "Select nombreAsignatura FROM alumnos_asignatura WHERE nombre = ?";
         try {
@@ -73,8 +75,8 @@ public class ControladorNotasALumnos {
             ResultSet rs = st.executeQuery();
 
             //Mientras que los resultados tengan datos, guarda los mismos en las diferentes variables y crea un objeto alumno donde almacena esos datos obtenidos
-            while (rs.next()){
-                String nombres= rs.getString("nombreAsignatura");
+            while (rs.next()) {
+                String nombres = rs.getString("nombreAsignatura");
                 notas.add(nombres);
             }
         } catch (SQLException throwables) {
@@ -83,8 +85,8 @@ public class ControladorNotasALumnos {
         return notas;
     }
 
-    public ArrayList <Integer> getNotasalumnos(int id_alumno){
-        ArrayList <Integer> notas = new ArrayList<Integer>();
+    public ArrayList<Integer> getNotasalumnos(int id_alumno) {
+        ArrayList<Integer> notas = new ArrayList<Integer>();
 
 
         String SQL = "Select notas FROM notas WHERE id_alumno = ?";
@@ -92,14 +94,14 @@ public class ControladorNotasALumnos {
             //Conecta con la base de datos y realiza a la misma la consulta correspondiente
             PreparedStatement st = conn.prepareStatement(SQL);
             //Ingresa el dato faltante en la consulta, reemplazando el signo de pregunta por el dato del id_usuario que le envia la persona
-            st.setInt(1,id_alumno);
+            st.setInt(1, id_alumno);
 
             //Obtiene los resultados
             ResultSet rs = st.executeQuery();
 
             //Mientras que los resultados tengan datos, guarda los mismos en las diferentes variables y crea un objeto alumno donde almacena esos datos obtenidos
-            while (rs.next()){
-                int idAlumno= rs.getInt("notas");
+            while (rs.next()) {
+                int idAlumno = rs.getInt("notas");
                 notas.add(idAlumno);
             }
         } catch (SQLException throwables) {
@@ -108,18 +110,18 @@ public class ControladorNotasALumnos {
         return notas;
     }
 
-    public   Alumno getAlumno(int idAlumno){
+    public Alumno getAlumno(int idAlumno) {
         String SQL = "Select nombre FROM alumno WHERE id_alumno = ?";
         try {
             //Conecta con la base de datos y realiza a la misma la consulta correspondiente
             PreparedStatement st = conn.prepareStatement(SQL);
             //Ingresa el dato faltante en la consulta, reemplazando el signo de pregunta por el dato del id_usuario que le envia la persona
-            st.setInt(1,idAlumno);
+            st.setInt(1, idAlumno);
             //Obtiene los resultados
             ResultSet rs = st.executeQuery();
 
             //Mientras que los resultados tengan datos, guarda los mismos en las diferentes variables y crea un objeto alumno donde almacena esos datos obtenidos
-            while (rs.next()){
+            while (rs.next()) {
                 String nombre = rs.getString("nombre");
                 return new Alumno(nombre, idAlumno);
             }
@@ -128,7 +130,28 @@ public class ControladorNotasALumnos {
         }
         return null;
     }
-    private Asignatura getAsignatura(int id_asignatura){
+    public Alumno getAlumnoid(String nombre) {
+        String SQL = "Select id_alumno FROM alumno WHERE nombre = ?";
+        try {
+            //Conecta con la base de datos y realiza a la misma la consulta correspondiente
+            PreparedStatement st = conn.prepareStatement(SQL);
+            //Ingresa el dato faltante en la consulta, reemplazando el signo de pregunta por el dato del id_usuario que le envia la persona
+            st.setString(1, nombre);
+            //Obtiene los resultados
+            ResultSet rs = st.executeQuery();
+
+            //Mientras que los resultados tengan datos, guarda los mismos en las diferentes variables y crea un objeto alumno donde almacena esos datos obtenidos
+            while (rs.next()) {
+                int idAlumno = rs.getInt("id_alumno");
+                return new Alumno(nombre, idAlumno);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+    public Asignatura getAsignatura(int id_asignatura) {
         //Conecta con la base de datos y realiza a la misma la consulta correspondiente
         String SQL = "Select nombre FROM asignatura WHERE id_asignatura = ?";
         try {
@@ -139,7 +162,7 @@ public class ControladorNotasALumnos {
             ResultSet rs = st.executeQuery();
 
             //Mientras que los resultados tengan datos, guarda los mismos en las diferentes variables y crea un objeto Asignatura donde almacena esos datos obtenidos
-            while (rs.next()){
+            while (rs.next()) {
                 String nombre = rs.getString("nombre");
                 return new Asignatura(nombre, id_asignatura);
             }
