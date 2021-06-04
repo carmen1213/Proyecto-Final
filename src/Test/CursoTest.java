3e222f4c00940c7ba288d61a593ede1999db851b
lucia816 <73232190+lucia816@users.noreply.github.com>
2021-06-04 20:59:35 +0200

    Junit curso test

--- src/Test/CursoTest.java
+++ src/Test/CursoTest.java
@@ -1,5 +1,7 @@
 package Test;
 
+import models.Alumno;
+import models.Curso;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,17 +10,29 @@ class CursoTest {
 
     @Test
     void getNombre_curso() {
+        Curso instancia = new Curso("Economia","Pepe");
+        instancia.getNombre_curso();
+        System.out.println(instancia.getNombre_curso());
     }
 
     @Test
     void setNombre_curso() {
+        Curso instancia = new Curso("Sociales","juan");
+        instancia.setNombre_curso("Naturales");
+        System.out.println("setNombre_curso()");
     }
 
     @Test
     void getNombre_alumno() {
+        Curso instancia = new Curso("Salvador","Lula");
+        instancia.getNombre_alumno();
+        System.out.println(instancia.getNombre_alumno());
     }
 
     @Test
     void setNombre_alumno() {
+        Curso instancia = new Curso("Arte","Juan");
+        instancia.setNombre_alumno("Lucas");
+        System.out.println("setNombre_alumno()");
     }
 }
